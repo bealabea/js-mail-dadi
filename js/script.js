dc98e96bb45684a641664f52d8e411d848998be9
bealabea <beatricemarin89@yahoo.it>
2021-12-13 15:50:47 +0100

    JS instructions for checking emails and generating random numbers

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// EMAIL
+
+// creo l'ARRAY delle email
+// chiedo all'utente la sua email con un PROMPT
+// creo una variabile (flag) con valore FALSE da attribuire in base al risultato che otterrò dal controllo
+
+// creo un ciclo FOR che esegua il controllo dell'email, se l'email dell'utente corrisponde ad una di quelle dell'array modificherò mia variabile (flag) in TRUE.
+// chiuso il ciclo for, SE l'email dell'utente risulta "vera" sarà presemte all'interno della lista
+// ALTRIMENTI sarà "falsa" e quindi non sarà presente all'interno della lista
+
+
+// DADI
+
+// attribuisco un numero casuale all'utente e uno al computer con due variabili
+// creo un ciclo di IF, SE la prima variabile è maggiore della seconda vince l'utente, ALTRIMENTI vince il computer
